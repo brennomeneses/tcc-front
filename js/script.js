a64ebb15047e9b1e845a7964baa85249b1344ac0
andersonneumann <anderson.lara@tecpuc.com.br>
2023-08-31 22:09:30 -0300

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  updateSystemTime();
+});
+
+function updateSystemTime(){
+  var weekdays = ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado'];
+
+  const date = new Date();
+  const hour = date.getHours().toString().padStart(2, '0');
+  const min = date.getMinutes().toString().padStart(2, '0');
+
+  var day = String(date.getDate()).padStart(2, '0');
+  var month = String(date.getMonth() + 1).padStart(2, '0'); // Month is zero-based
+  var year = date.getFullYear();
+  var formattedDate = day + '/' + month + '/' + year;
+
+  $("#currentDay").html(weekdays[date.getDay()] + " - " + formattedDate);
+  $("#currentTime").html(hour + ":" + min);
+
+}

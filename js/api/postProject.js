9df807ebc07d4a0eca0cfab3c0168215e74ffa41
brennomeneses <brennocorps@gmail.com>
2023-09-21 22:08:30 -0300

    Session Storage creation project doing

--- js/api/postProject.js
+++ js/api/postProject.js
@@ -1,7 +1,27 @@
 window.addEventListener("DOMContentLoaded", async ()=> {
-  try {
-    
-  } catch (error) {
-    console.log(error)
-  }
+    sessionStorage.removeItem("app:ProjectCache")
+
+    document.getElementById('basicInfoProject').addEventListener('submit', (event) => {
+      event.preventDefault()
+  
+      const formData = new FormData(event.target)
+
+      const formValues = Object.fromEntries(formData)
+
+      sessionStorage.setItem("app:ProjectCache", JSON.stringify({
+        name: formValues.newProjectName,
+        description: formValues.newProjectDescription,
+        stakeholder: formValues.newProjectStakeholder,
+        cost: formValues.newProjectEstimatedCost,
+        projectManager: formValues.newProjectSelectProjectManager,
+        deadline: formValues.newProjectEstimatedDuration,
+        dice: {
+          teamCapacity: formValues.newProjectDiceTeamCapacity,
+          stakeholderCommitment: formValues.newProjectDiceStakeholderCommitment,
+          futureUsersCommmitment: formValues.newProjectDiceFutureUsersCommitment
+        }
+      }))
+
+    console.log(formValues)
+    })
 })
